15849f3490e37690bd7cab8ca8b46be9e46dbd31
LehongWu <133838847+LehongWu@users.noreply.github.com>
2023-05-29 13:25:59 +0800

    wlh updated game2

---
+++ GameMap/GameMap2.3/level_page_base/mylabel.cpp
@@ -0,0 +1,18 @@
+#include "mylabel.h"
+
+MyLabel::MyLabel(QWidget* parent, const QString& text)
+    :QLabel(text, parent)
+{
+    QFont temp("Microsoft Yi Baiti",18,QFont::Bold);
+    setMargin(0);
+    setAlignment(Qt::AlignCenter);
+    setFont(temp);
+}
+
+void MyLabel::resizeEvent(QResizeEvent* event)
+{
+    QWidget::resizeEvent(event);
+    auto temp = font();
+    temp.setPixelSize(height()*2/3);
+    setFont(temp);
+}
