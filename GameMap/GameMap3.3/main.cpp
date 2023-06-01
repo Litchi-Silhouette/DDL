29f69a0298e31253d9f3d5a8d4b48989b6d85567
LehongWu <133838847+LehongWu@users.noreply.github.com>
2023-06-01 22:35:53 +0800

    wlh updated game3

---
+++ GameMap/GameMap3.3/main.cpp
@@ -0,0 +1,20 @@
+#include "init.cpp"
+#include "obstacle.cpp"
+
+
+Game LevelWindow::gamePages;
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+
+    MainWindow *pwindow = new MainWindowThree();
+
+    qDebug()<<"MainWindow size "<<pwindow->height()<<" "<<pwindow->width();
+    pwindow->show();
+
+
+
+    return a.exec();
+}
